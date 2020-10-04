4cfab0bbf50bb4443adc684e2e524d580cd6a6ce
pamungkasaji <pamungkasaji1997@gmail.com>
2020-10-04 13:28:54 +0700

    first commit

---
+++ src/main/java/com/pamungkasaji/donasi/DonasiApplication.java
@@ -0,0 +1,13 @@
+package com.pamungkasaji.donasi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DonasiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DonasiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pamungkasaji/donasi/DonasiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pamungkasaji.donasi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DonasiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
